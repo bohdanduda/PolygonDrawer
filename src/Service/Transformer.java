6717afad07375ae2468320439579ea610df7089c
Bohdan <duda.bohdan23@gmail.com>
2024-05-11 17:35:48 +0200

    Create service method for transforming point in text to object

--- src/Service/Transformer.java
+++ src/Service/Transformer.java
@@ -1,6 +1,7 @@
 package Service;
 
 import Model.Dimensions;
+import Model.Point;
 
 public class Transformer {
     public Dimensions transformDimensions(String dimensionsInText) {
@@ -15,5 +16,55 @@ public class Transformer {
         }
 
         return new Dimensions(Integer.parseInt(dimensions[0]), Integer.parseInt(dimensions[1]));
+    }
+
+    public Point transformPoint(String pointInText, Dimensions dimensions) {
+        String[] arrPoints = pointInText.split(",");
+
+        if (arrPoints.length != 2) {
+            throw new IllegalArgumentException("Invalid dimension format. Expected format: <width>x<height>");
+        }
+
+        String x = arrPoints[0];
+        String y = arrPoints[1];
+
+        int xValue = calculateCoordinate(x, dimensions.getWidth(), 'L', 'C', 'R');
+        int yValue = calculateCoordinate(y, dimensions.getHeight(), 'T', 'C', 'B');
+
+        return new Point(xValue, yValue);
+    }
+
+    private int calculateCoordinate(String coordinate, int maxValue, char defaultRef, char centerRef, char endRef) {
+        char reference = defaultRef;
+        int value;
+        int center = Math.round((float) maxValue / 2);
+
+        if (Character.isLetter(coordinate.charAt(0))) {
+            if (coordinate.charAt(0) != 'L' && coordinate.charAt(0) != 'T' && coordinate.charAt(0) != 'C' && coordinate.charAt(0) != 'R' && coordinate.charAt(0) != 'B') {
+                throw new IllegalArgumentException("Invalid default reference. Expected values: L, T, C, R, B");
+            }
+            reference = coordinate.charAt(0);
+            value = Integer.parseInt(coordinate.substring(1));
+        } else {
+            value = Integer.parseInt(coordinate);
+        }
+
+        if (reference == defaultRef && value < 0) {
+            throw new IllegalArgumentException("Invalid coordinate");
+        }
+
+        if (reference == endRef && value > 0) {
+            throw new IllegalArgumentException("Invalid coordinate");
+        }
+
+        if (reference == defaultRef) {
+            return value;
+        }
+
+        if (reference == centerRef) {
+            return center + value;
+        }
+
+        return maxValue + value;
     }
 }
--- test/TransformerTest.java
+++ test/TransformerTest.java
@@ -1,4 +1,5 @@
 import Model.Dimensions;
+import Model.Point;
 import Service.Transformer;
 import org.junit.jupiter.api.Test;
 
@@ -29,6 +30,72 @@ public class TransformerTest {
         Transformer transformer = new Transformer();
         assertThrows(IllegalArgumentException.class, () -> {
             transformer.transformDimensions("-10x-20");
+        });
+    }
+
+    @Test
+    public void testTransformPoint_DefaulRef() {
+        Transformer transformer = new Transformer();
+        Dimensions dimensions = new Dimensions(10, 20);
+        Point expected = new Point(0, 10);
+        Point actual = transformer.transformPoint("0,10", dimensions);
+        assertEquals(expected.getX(), actual.getX());
+        assertEquals(expected.getY(), actual.getY());
+    }
+
+    @Test
+    public void testTransformPoint_CenterRef() {
+        Transformer transformer = new Transformer();
+        Dimensions dimensions = new Dimensions(10, 20);
+        Point expected = new Point(10, 5);
+        Point actual = transformer.transformPoint("C5,C-5", dimensions);
+        assertEquals(expected.getX(), actual.getX());
+        assertEquals(expected.getY(), actual.getY());
+    }
+
+    @Test
+    public void testTransformPoint_EndRef() {
+        Transformer transformer = new Transformer();
+        Dimensions dimensions = new Dimensions(10, 20);
+        Point expected = new Point(5, 15);
+        Point actual = transformer.transformPoint("R-5,B-5", dimensions);
+        assertEquals(expected.getX(), actual.getX());
+        assertEquals(expected.getY(), actual.getY());
+    }
+
+    @Test
+    public void testTransformPoint_DefaulRef_Invalid() {
+        Transformer transformer = new Transformer();
+        Dimensions dimensions = new Dimensions(10, 20);
+        assertThrows(IllegalArgumentException.class, () -> {
+            transformer.transformPoint("-5,-10", dimensions);
+        });
+    }
+
+    @Test
+    public void testTransformPoint_CenterRef_Invalid() {
+        Transformer transformer = new Transformer();
+        Dimensions dimensions = new Dimensions(10, 20);
+        assertThrows(IllegalArgumentException.class, () -> {
+            transformer.transformPoint("C10, C-20", dimensions);
+        });
+    }
+
+    @Test
+    public void testTransformPoint_EndRef_Invalid() {
+        Transformer transformer = new Transformer();
+        Dimensions dimensions = new Dimensions(10, 20);
+        assertThrows(IllegalArgumentException.class, () -> {
+            transformer.transformPoint("R5,B5", dimensions);
+        });
+    }
+
+    @Test
+    public void testTransformPointWithInvalidFormat() {
+        Transformer transformer = new Transformer();
+        Dimensions dimensions = new Dimensions(10, 20);
+        assertThrows(IllegalArgumentException.class, () -> {
+            transformer.transformPoint("5-10", dimensions);
         });
     }
 }
